000dbb5ed960c75fd9fed5a7f86a8e75f06faead
Mahboob Alam <connect.mahboobalam@gmail.com>
2026-09-03 10:15:09 +0000

    fix(isolation): grant standard character devices in Landlock defaults
    - Allow read access to /dev/urandom and /dev/random for CSPRNG
    - Allow read/write access to /dev/null and /dev/zero for standard POSIX streams
    - Fixes Permission Denied errors when applications read /dev/urandom or redirect to /dev/null
    
    Implements: FR8
    Task: allow-dev-null-and-urandom-in-landlock

--- internal/isolation/landlock.go
+++ internal/isolation/landlock.go
@@ -45,6 +45,8 @@ func ApplyLandlock(allowPathsRO []string, allowPathsRW []string, allowFilesRW []
 	roDirs := filterExisting(allRODirs)
 
 	roFiles := filterExisting([]string{
+		"/dev/urandom",
+		"/dev/random",
 		"/etc/ld.so.cache",
 		"/etc/ld.so.conf",
 		"/etc/nsswitch.conf",
@@ -69,7 +71,12 @@ func ApplyLandlock(allowPathsRO []string, allowPathsRW []string, allowFilesRW []
 		}
 	}
 
-	filteredFilesRW := filterExisting(allowFilesRW)
+	defaultRWFiles := filterExisting([]string{
+		"/dev/null",
+		"/dev/zero",
+	})
+	allRWFiles := append(defaultRWFiles, allowFilesRW...)
+	filteredFilesRW := filterExisting(allRWFiles)
 	for _, p := range allowFilesRW {
 		found := false
 		for _, f := range filteredFilesRW {
@@ -127,6 +134,8 @@ func ProbeLandlock(allowPathsRO, allowPathsRW, allowFilesRW []string) (ProbeRepo
 	roDirs := filterExisting(allRODirs)
 
 	roFiles := filterExisting([]string{
+		"/dev/urandom",
+		"/dev/random",
 		"/etc/ld.so.cache",
 		"/etc/ld.so.conf",
 		"/etc/nsswitch.conf",
@@ -138,7 +147,12 @@ func ProbeLandlock(allowPathsRO, allowPathsRW, allowFilesRW []string) (ProbeRepo
 	filteredRW := filterExisting(allowPathsRW)
 	rwPaths := append([]string{cwd}, filteredRW...)
 
-	filteredFilesRW := filterExisting(allowFilesRW)
+	defaultRWFiles := filterExisting([]string{
+		"/dev/null",
+		"/dev/zero",
+	})
+	allRWFiles := append(defaultRWFiles, allowFilesRW...)
+	filteredFilesRW := filterExisting(allRWFiles)
 
 	return ProbeReport{
 		WorkingDir:       cwd,
